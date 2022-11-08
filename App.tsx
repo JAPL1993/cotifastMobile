aebb24dc84f8c30b9e40bafa1f276822384cf000
JAPL <andrespuerto1993@hotmail.com>
2022-11-08 09:17:11 -0600

    fitdy commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,29 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, useWindowDimensions } from 'react-native';
+import 'react-native-gesture-handler';
+import { Canvas, Fill, Box, BoxShadow, rrect, rect } from "@shopify/react-native-skia";
+import { colorSchema } from "./enums/colorSchema";
 
 export default function App() {
+  const { width, height } = useWindowDimensions()
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Canvas style={{ width, height, position: 'absolute' }}>
+        <Fill color={colorSchema.main} />
+        <Box box={rrect(rect(width / 2 - 64, 64, 128, 128), 24, 24)} color={colorSchema.main}>
+          <BoxShadow dx={10} dy={10} blur={10} color={colorSchema.lShadow} inner />
+          <BoxShadow dx={-10} dy={-10} blur={10} color={colorSchema.dShadow} inner />
+          <BoxShadow dx={10} dy={10} blur={10} color={colorSchema.lShadow} />
+          <BoxShadow dx={-10} dy={-10} blur={10} color={colorSchema.dShadow} />
+        </Box>
+        <Box box={rrect(rect(width / 2 - 64, height / 2 - 64, 128, 128), 100, 100)} color={colorSchema.main}>
+          <BoxShadow dx={10} dy={10} blur={10} color={colorSchema.lShadow} inner />
+          <BoxShadow dx={-10} dy={-10} blur={10} color={colorSchema.dShadow} inner />
+          <BoxShadow dx={10} dy={10} blur={10} color={colorSchema.lShadow} />
+          <BoxShadow dx={-10} dy={-10} blur={10} color={colorSchema.dShadow} />
+        </Box>
+      </Canvas>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ enums/colorSchema.ts
@@ -0,0 +1,5 @@
+export enum colorSchema {
+    main = '#E0E5EC',
+    lShadow = '#FFFFFF',
+    dShadow = '#A3B1C6'
+}
